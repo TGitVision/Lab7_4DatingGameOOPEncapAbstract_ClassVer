57877301d982f455c77f5fe1e44df055fec80000
TGitVision <60406685+TGitVision@users.noreply.github.com>
2020-07-07 13:48:00 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab7_4DatingGameOOPEncapAbstract_ClassVer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Lab7_4DatingGameOOPEncapAbstract_ClassVer!");
+        }
+    }
+}
