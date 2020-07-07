09e995d1476d2f1210e08e6e8065dfcb7d584842
TGitVision <60406685+TGitVision@users.noreply.github.com>
2020-07-07 14:19:02 -0400

    Created Class Version of the DatingGame project. Beta version...Not complete.

--- DatingProfile.cs
+++ DatingProfile.cs
@@ -6,5 +6,33 @@ namespace Lab7_4DatingGameOOPEncapAbstract_ClassVer
 {
     class DatingProfile
     {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public string bio { get; set; }
+        public DatingProfile(string firstName, string lastName, int age, string gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public void WriteBio(string inputBio) 
+        {
+            this.bio = inputBio;
+        }
+        public void ReadMessage() 
+        {
+            
+        }
+        public void SendMessage(string MessageTitle, string MessageData, DatingProfile toProfile) 
+        { 
+        
+        }
+        
+
     }
+
 }
---
+++ Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7_4DatingGameOOPEncapAbstract_ClassVer
+{
+    class Message
+    {
+        public DatingProfile sender { get; set; }
+        public string messageTitle { get; set; }
+        public string messageData { get; set; }
+        private bool isRead { get; set; }
+        public Message(DatingProfile sender, string messageTitle, string messageData)
+        {
+            this.sender = sender;
+            this.messageTitle = messageTitle;
+            this.messageData = messageData;
+            this.isRead = false;
+        }
+    }
+}
